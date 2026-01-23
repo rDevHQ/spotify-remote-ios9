83f3c6c707aa1a4db6359b7b1b4ea064298bee4f
rDevHQ <ragonline@pm.me>
2026-01-23 12:31:28 +0100

    Use config-based device grid

---
+++ public/sonos-devices.js
@@ -0,0 +1,5 @@
+window.SONOS_DEVICES = [
+  { "id": "kitchen", "name": "Kitchen", "entity_id": "media_player.kitchen_sonos" },
+  { "id": "livingroom", "name": "Living Room", "entity_id": "media_player.livingroom_sonos" },
+  { "id": "bedroom", "name": "Bedroom", "entity_id": "media_player.bedroom_sonos" }
+];
