fc4f5fa4e5e629dc818595f3988e99c2a090cf35
rDevHQ <ragonline@pm.me>
2026-01-24 14:23:20 +0100

    Auto-detect master speaker from Spotify API

--- public/sonos-devices.js
+++ public/sonos-devices.js
@@ -1,5 +1,5 @@
 window.SONOS_DEVICES = [
-  { "id": "kitchen", "name": "Kitchen", "entity_id": "media_player.kitchen_sonos" },
-  { "id": "livingroom", "name": "Living Room", "entity_id": "media_player.livingroom_sonos" },
-  { "id": "bedroom", "name": "Bedroom", "entity_id": "media_player.bedroom_sonos" }
+  { "id": "kitchen", "name": "Kitchen", "entity_id": "media_player.kitchen_sonos", "spotify_name": "Sonos Kök" },
+  { "id": "livingroom", "name": "Living Room", "entity_id": "media_player.livingroom_sonos", "spotify_name": "Sonos Vardagsrum" },
+  { "id": "bedroom", "name": "Bedroom", "entity_id": "media_player.bedroom_sonos", "spotify_name": "Sonos Sovrum" }
 ];
